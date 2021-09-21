f97975ce13def38df8285a2702a50f912e27784a
dylangangat <dylan.gangat@gmail.com>
2021-09-21 23:55:34 +0200

    Added the html markup for home, city, products and product-info pages

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Open Sans", "sans-serif"],
+      },
+      textColor: {
+        primary: "#343a40",
+        secondary: "#f95356",
+        accent: "#284477",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
